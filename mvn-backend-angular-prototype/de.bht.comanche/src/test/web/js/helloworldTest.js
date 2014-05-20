e8d46b9c68e23f3bc487a4504efc0c4c603efc0a
sebastian-dasse <s50602@beuth-hochschule.de>
2014-05-21 00:03:45 +0200

    Mavenize angular. Create merged pom.xml for backend plus angular. mvn site not working properly.

---
+++ mvn-backend-angular-prototype/de.bht.comanche/src/test/web/js/helloworldTest.js
@@ -0,0 +1,8 @@
+describe('A suite',function(){
+    
+    it('says Hello World!',function(){
+        expect(Hello.sayHelloWorld()).toBe("Hello World!");
+        // expect(Hello.sayHelloWorld()).toEqual("Hello World!");
+    });
+
+});
