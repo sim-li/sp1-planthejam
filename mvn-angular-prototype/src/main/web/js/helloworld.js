92940bf187a90cb0109ce3b629596cf885f9ff3d
sebastian-dasse <s50602@beuth-hochschule.de>
2014-05-21 14:16:01 +0200

    Add JUnit test example. Fix jetty warning about missing version.

--- mvn-angular-prototype/src/main/web/js/helloworld.js
+++ mvn-angular-prototype/src/main/web/js/helloworld.js
@@ -1,6 +1,13 @@
+/**
+ * The controller for the angular app
+ * @namespace
+ */
 var Hello = {
     //"use strict";
     
+    /**
+     * @return the string "Hello World!"
+     */
     sayHelloWorld: function() {
         return "Hello World!";
         // console.log("Hello World!");
