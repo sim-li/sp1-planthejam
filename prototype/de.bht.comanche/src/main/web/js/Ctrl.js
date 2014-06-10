7af8baf99c80b681c48a44dc1144762dfb3ce03e
sebastian-dasse <s50602@beuth-hochschule.de>
2014-06-10 02:07:05 +0200

    Objektverwaltung-UI pt.1

--- prototype/de.bht.comanche/src/main/web/js/Ctrl.js
+++ prototype/de.bht.comanche/src/main/web/js/Ctrl.js
@@ -1,5 +1,5 @@
 /*
- * Technik-Prototyp, SP1 SoSe 2014, Team: Comanche
+ * Ojektverwaltung-UI, SP1 SoSe 2014, Team: Comanche
  * (C)opyright Sebastian Dassé, Mat.-Nr. 791537, s50602@beuth-hochschule.de
  * 
  * Module: controller
@@ -16,87 +16,88 @@ var Ctrl = (function() {
     var myApp = angular.module("myApp", []);
     myApp.controller("Ctrl", function($scope) {
 
-        $scope.allowedPIN = /^\d{5}$/;
+        // $scope.allowedPassword = /^\d{5}$/;
+        $scope.allowedPassword = /^[^\s]{8,20}$/; // no whitespace allowed -- TODO: at this point whitespace is still allowed at the beginning and end
+        // $scope.allowedPassword = /^[^\s]+$/; // no whitespace allowed -- TODO: at this point whitespace is still allowed at the beginning and end
 
 
-        $scope.reverseName = function() {
-            return $scope.secrets && $scope.secrets.split('').reverse().join('');
+        var initUser = function() {
+
+            // *** get user data from server ***
+
+            var dummyList = [
+                { "name": "Bandprobe", 
+                  "description": "Wir müssen vor dem Konzert Ende des Monats mindestens noch einmal proben. Wann könnt ihr?", 
+                  "type": "UNIQUE", // or "RECURRING" <<enumeration>> = einmalig oder wiederkehrend
+                  "deadline": "04.07.2014, 23:55", // <<datatype>> date = Zeipunkt
+                  "frequency": { "distance": 0, "timeUnit": "WEEK" }, // <<datatype>> iteration = Wiederholung
+                  "possibleTimeslots": [ // <<datatype>> List<timeslot> = List<Zeitraum>
+                        { "startTime": "11.07.2014, 19:00", "durationInMins": 120 }, 
+                        { "startTime": "12.07.2014, 20:00", "durationInMins": 120 }, 
+                        { "startTime": "18.07.2014, 19:30", "durationInMins": 120 } ], 
+                  "determinedTimeslot": { "startTime": "12.07.2014, 20:00", "durationInMins": 120 } }, // <<datatype>> timeslot = Zeitraum
+                { "name": "Chorprobe" }, 
+                { "name": "Meeting" }];
+
+            return {
+                "name": "", 
+                "password": "", 
+                "loggedIn": false, 
+                "surveys": dummyList // *** replace list of dummy surveys by real data from server ***
+            };
         };
+        
+        $scope.user = initUser();
 
 
-        $scope.show = {
-            cardGame: true, 
-            calculator: false, 
-            somethingElse: false, 
-            suitSelection: false, 
-            cardSelection: false
-        };
+        $scope.login = function() {
+            $scope.warning = "";
 
-        $scope.togglePlayground = function(x) {
-            $scope.show.cardGame = false;
-            $scope.show.calculator = false;
-            $scope.show.somethingElse = false;
+            if (!$scope.user.name || !$scope.user.password) {
+                console.log("Login fehlgeschlagen.");
+                console.log($scope.user);
 
-            switch(x) {
-            case "CARD_GAME":
-                $scope.show.cardGame = true;
-                break;
-            case "CALCULATOR":
-                $scope.show.calculator = true;
-                break;
-            case "SOMETHING_ELSE":
-                $scope.show.somethingElse = true;
-                break;
-            default:
-                throw new Error("This case should not happen.");
+                if (!$scope.user.name) {
+                    console.log("Benutzername fehlt.");
+                }
+                if (!$scope.user.password) {
+                    console.log("Passwort fehlt.");
+                }
+            } else if (!$scope.allowedPassword.test($scope.user.password)) {
+                $scope.warning = "Das Passwort muss 8-20 Zeichen lang sein und darf keine Leerzeichen enthalten!";
+                console.log($scope.warning);
+            } else {
+                
+                // *** try to login at server ***
+                
+                $scope.user.loggedIn = true;
+                console.log("Login erfolgreich. Benutzer:");
+                console.log($scope.user);
             }
         };
+        
+        // $scope.test_ = function() {
+        //     if (!$scope.allowedPassword.test($scope.user.password)) {
+        //         $scope.warning = "Das Passwort muss 8-20 Zeichen lang sein und darf keine Leerzeichen enthalten!";
+        //         console.log($scope.warning);
+        //     } else {
+        //         $scope.warning = "";
+        //     }
+        // }
 
-        $scope.toggleSelection = function(x) {
-            switch(x) {
-                case "SUIT_SELECTION":
-                    $scope.show.suitSelection = !$scope.show.suitSelection;
-                    $scope.show.cardSelection = false;
-                    break;
-                case "CARD_SELECTION":
-                    $scope.show.cardSelection = !$scope.show.cardSelection;
-                    $scope.show.suitSelection = false;
-                    break;
-                default:
-                    throw new Error("This case should not happen.");
-            }
-        };
-
-
-        $scope.cards = {
-            suit: ["Karo", "Herz", "Pik", "Kreuz"], 
-            card: ["Sieben", "Acht", "Neun", "Zehn", "Bube", "Dame", "K\u00f6nig", "As"]
+        $scope.test_ = function() {
+            $scope.warning = "";
         }
 
-        $scope.test = { 
-            update: function() {
-                this.suit = randomElementFrom($scope.cards.suit); 
-                this.card = randomElementFrom($scope.cards.card); 
-                this.date = new Date().toLocaleTimeString();
-            }
-        };
-        $scope.test.update();
-        
+        $scope.logout = function() {
 
-        $scope.add = function() {
-            $scope.sum = parseInt($scope.a) + parseInt($scope.b);
+            // *** try to logout at server ***
+
+            $scope.user = initUser();
+            console.log("Logout erfolgreich.");
+            console.log($scope.user);
         };
 
     });
-    
-    /**
-     * Selects a random element from the given array
-     * @param array_ an array
-     * @return a random element from the given array
-     */
-    var randomElementFrom = function(array_) {
-        var i = parseInt(Math.random() * array_.length);
-        return array_[i];
-    };
 
 }());
