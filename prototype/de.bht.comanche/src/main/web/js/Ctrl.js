3bbba11a5b767174ce58475a88cebf6794a758a2
sebastian-dasse <s50602@beuth-hochschule.de>
2014-06-12 02:27:56 +0200

    A better object management UI.

--- prototype/de.bht.comanche/src/main/web/js/Ctrl.js
+++ prototype/de.bht.comanche/src/main/web/js/Ctrl.js
@@ -121,8 +121,8 @@ var Ctrl = (function() {
                 console.log($scope.user);
 
                 // TODO: Baustelle -- checken, ob so sinnvoll:
-                $scope.filteredSurveys = $scope.user.surveys;
-                $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
+                // $scope.filteredSurveys = $scope.user.surveys;
+                // $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
             }
         };
 
@@ -139,25 +139,89 @@ var Ctrl = (function() {
             $scope.warning = "";
         };
 
-        // TODO: Baustelle
-        $scope.updateSelection = function() {
-            $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
+
+        var Survey = function(config) {
+            config = config || {};
+            this.name = config.name || "";
+            this.description = config.description || "";
+            this.type = config.type || $scope.Type.UNIQUE;
+            this.deadline = config.deadline || new Date();
+            this.frequency = config.frequency || { "distance": 0, "timeUnit": $scope.TimeUnit.WEEK };
+            this.possibleTimeslots = config.possibleTimeslots || [ { "startTime": new Date(), "durationInMins": 60 } ], 
+            this.determinedTimeslot = config.determinedTimeslot || {}
+        };
+
+        $scope.editSurvey = function() {
+            $scope.user.tempSurvey = new Survey($scope.user.selectedSurvey);
+            $scope.user.inEditMode = true;
+        };
+
+        $scope.addSurvey = function() {
+            $scope.user.tempSurvey = new Survey();
+            $scope.user.adding = true;
+            $scope.user.inEditMode = true;
+        };
+
+        $scope.cancelEditSurvey = function() {
+            $scope.user.tempSurvey = "";
+            $scope.user.adding = false;
+            $scope.user.inEditMode = false;
+        };
+
+        $scope.saveSurvey = function() {
+            
+            // *** try to synchronize with server ***
+            
+            if (!$scope.user.adding) {
+                removeSelectedSurvey();
+            }
+            $scope.user.surveys.push($scope.user.tempSurvey);
+            $scope.user.surveys.sort(function(a, b){ return a.name.localeCompare(b.name) });
+
+
+            $scope.user.selectedSurvey = $scope.user.tempSurvey;
+            $scope.user.tempSurvey = "";
+            $scope.user.adding = false;
+            $scope.user.inEditMode = false;
         };
 
 
-        $scope.remove = function(item) {
-            var index = $scope.user.surveys.indexOf(item);
-            $scope.user.surveys.splice(index, 1);
+
+        // TODO: Baustelle
+        // $scope.updateSelection = function() {
+        //     $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
+        // };
+
+
+        var removeElementFrom = function(element, array) {
+            var index = array.indexOf(element);
+            if (index > -1) {
+                array.splice(index, 1);
+            }
+        };
+
+        var removeSelectedSurvey = function() {
+            removeElementFrom($scope.user.selectedSurvey, $scope.user.surveys);
+            $scope.user.selectedSurvey = "";
         };
 
         $scope.deleteSelectedSurvey = function() {
 
             // *** ask: are you sure you want to delete? ***
             // *** try to delete from server ***
+            // *** refresh local model ***
 
-            $scope.remove($scope.user.selectedSurvey);
-            $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
+            removeSelectedSurvey();
+            // $scope.user.selectedSurvey = $scope.filteredSurveys[0] || "";
             console.log($scope.user);
+        };
+
+        $scope.removeTimeSlotFromTempSurvey = function(timeslot) {
+            removeElementFrom(timeslot, $scope.user.tempSurvey.possibleTimeslots);
+        };
+
+        $scope.addTimeSlotToTempSurvey = function() {
+            $scope.user.tempSurvey.possibleTimeslots.push({ "startTime": new Date(), "durationInMins": 60 });
         };
 
         
@@ -176,5 +240,12 @@ var Ctrl = (function() {
         // };
 
     });
+
+    // Ctrl.prototype.remove = function(first_argument) {
+    //     // body...
+    //     console.log($scope.user.name);
+    // };
+
+    // return Ctrl;
 
 }());
