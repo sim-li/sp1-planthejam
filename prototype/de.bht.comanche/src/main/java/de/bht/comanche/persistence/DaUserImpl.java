6b9b3bbe0830e5ea3245d458585317f06153ab14
sebastian-dasse <s50602@beuth-hochschule.de>
2014-06-22 11:59:05 +0200

    Add required constructor.

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DaUserImpl.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DaUserImpl.java
@@ -6,6 +6,11 @@ import de.bht.comanche.model.DmUser;
 
 public class DaUserImpl extends DaGenericImpl<DmUser> implements DaUser {
 
+	public DaUserImpl(Class<DmUser> type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public Collection<DmUser> findByName(String name) {
 		// TODO Auto-generated method stub
