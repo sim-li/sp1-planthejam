cd85b745871e98ee6955e72503d70763f05b118e
Simon Lischka <simon@a-studios.org>
2014-06-24 17:57:56 +0200

    Remove entity annotation from DBObject

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DbObject.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DbObject.java
@@ -1,24 +1,7 @@
 package de.bht.comanche.persistence;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 /**
  * A marker interface.
  *
  */
-@Entity
 public abstract class DbObject {
-    private Long id;
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    public Long getId() {
-    	return id;
-    }
-
-    private void setId(Long id) {
-    	this.id = id;
-    }
 }
