f58862ab9712041285cd6151d6a96fcbad004c03
Simon Lischka <simon@a-studios.org>
2014-06-25 17:29:19 +0200

    Missing file

---
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/logic/DtTimeperiod.java
@@ -0,0 +1,43 @@
+package de.bht.comanche.logic;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+//@Table (name = "\"users\"") ??
+public class DtTimeperiod {
+	private Date startTime;
+	private int durationInMinutes;
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+    	return getId();
+    }
+    
+    public void setId(Long id) {
+    	setId(id);
+    }
+
+	private Date getStartTime() {
+		return startTime;
+	}
+
+	private void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	private int getDurationInMinutes() {
+		return durationInMinutes;
+	}
+
+	private void setDurationInMinutes(int durationInMinutes) {
+		this.durationInMinutes = durationInMinutes;
+	}
+    
+    
+}
