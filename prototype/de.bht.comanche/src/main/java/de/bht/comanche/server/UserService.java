2b6d6be35a08833f78d80549ed2bde85c75b0ed5
Max Novichkov <s500@beuth-hochschule.de>
2014-07-05 17:52:04 +0200

    merge

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/server/UserService.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/server/UserService.java
@@ -10,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -20,6 +21,7 @@ import de.bht.comanche.logic.LgUser;
 import de.bht.comanche.persistence.DaUser;
 
 @Path("/user/")
+@Produces({"text/xml", "application/json"})
 public class UserService {
 	private DaUser dao;
 	
