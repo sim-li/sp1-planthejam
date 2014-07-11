efa4c625db5dcd5c42afa6edd4df984c1283051f
Sebastian Dassé <sebdas@web.de>
2014-07-11 18:16:14 +0200

    Start with some persistence tests.

--- prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/PersistenceTest.java
+++ prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/PersistenceTest.java
@@ -7,12 +7,19 @@ import java.util.Collection;
 import javax.persistence.EntityExistsException;
 import javax.transaction.TransactionRequiredException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import de.bht.comanche.logic.LgUser;
 
 public class PersistenceTest {
-
+	
+	private DaFactory factory;
+	
+	@Before public void setUp(){
+		factory = new JpaDaFactory();
+	}
+	
 	@Test public void saveUserTest() {
 		JpaDaFactory factory = new JpaDaFactory();
 		DaUser daUser = factory.getDaUser();
@@ -36,6 +43,33 @@ public class PersistenceTest {
 			e.printStackTrace();
 		}
 		daUser.endTransaction(ok);
+		assertEquals(ok, true);
+    }
+	
+	@Test public void saveUser2() {
+		DaUser daUser = factory.getDaUser();
+		
+		daUser.beginTransaction(); // FIXME
+		
+		LgUser lgUser = new LgUser();
+		lgUser.setName("Ralf");
+		lgUser.setEmail("test@user.tst");
+		lgUser.setPassword("nosafepwd");
+		lgUser.setTelephone("0301234567");
+		lgUser.setOid(111111111);
+//		lgUser.set
+		
+		boolean ok = false;
+		
+		try {
+			daUser.save(lgUser);
+			ok = true;
+		} catch (Exception  e) {
+			e.printStackTrace();
+		}
+		
+		daUser.endTransaction(ok); // FIXME
+		
 		assertEquals(ok, true);
     }
 	
