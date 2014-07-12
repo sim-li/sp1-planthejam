60b31cd33b90e7be6ff1cc7bbe63f217054654a8
Simon Lishka <simon@a-studios.org>
2014-07-12 16:40:16 +0200

    Changed UserService

---
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/server/exceptions/logic/UserWithThisNameExist.java
@@ -0,0 +1,20 @@
+package de.bht.comanche.server.exceptions.logic;
+
+import de.bht.comanche.server.exceptions.LogicErrorMessage;
+import de.bht.comanche.server.exceptions.LogicException;
+
+
+public class UserWithThisNameExist extends LogicException {
+
+	private static final long serialVersionUID = -3481383568588952305L;
+	
+	public UserWithThisNameExist() {
+		super(LogicErrorMessage.USER_EXIST);
+	}
+}
+
+
+
+
+
+
