ea2ca09990f1de6926b56ec2acecc34025b920cc
Sebastian Dassé <sebdas@web.de>
2014-07-17 00:47:58 +0200

    Delete test/web.

--- prototype/de.bht.comanche/src/test/web/js/helloworldTest.js
+++
@@ -1,8 +0,0 @@
-describe('A suite',function(){
-
-    it('says Hello World!',function(){
-        expect(Hello.sayHelloWorld()).toBe("Hello World!");
-        // expect(Hello.sayHelloWorld()).toEqual("Hello World!");
-    });
-
-});
