ecdbd239de2b87c37db933681f32216b019d4877
Max <s50030@beuth-hochschule.de>
2014-10-15 16:15:57 +0200

    Merge

---
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/exceptions/ServerException.java
@@ -0,0 +1,20 @@
+package de.bht.comanche.exceptions;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+public class ServerException extends WebApplicationException {
+	
+	private static final long serialVersionUID = 1L;
+
+
+	public ServerException(ErrorMessage message) {
+		super(Response.serverError().entity(message).type(MediaType.APPLICATION_JSON).build());
+	}
+
+	public ServerException(int status, ErrorMessage message) {
+		super(Response.status(status).entity(message).type(MediaType.APPLICATION_JSON).build());
+	}
+	
+}
