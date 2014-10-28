e6313bf7c528090c4903964b25e30fffdbb439f3
Simon Lischka <simon@lischka.co>
2014-10-28 18:44:07 +0100

    Hibernate pool basic methods

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DaHibernateJpaPool.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/persistence/DaHibernateJpaPool.java
@@ -1,105 +1,85 @@
 package de.bht.comanche.persistence;
-
-import static multex.MultexUtil.create;
-
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import multex.Failure;
 
 public class DaHibernateJpaPool implements DaPool {
 	private final String persistenceUnitName = "planthejam.jpa";
-	private EntityManager em;
+	private EntityManager entityManager;
 	private EntityManagerFactory entityManagerFactory;
 
 	public DaHibernateJpaPool () {
-		entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
-	}
-
-    public EntityManager getEntityManager() {
-    	return em;
-    }
-    
-	@Override
-	public void save(DaObject io_object) {
-		em.persist(io_object);
-	}
-
-	@Override
-	public E merge(E io_object) {
-		return em.merge(io_object);
-	}
-
-	@Override
-	public void delete(E io_object) {
-		em.remove(em.contains(io_object) ? io_object : em.merge(io_object));
-	}
-
-	@Override
-	public <T extends DaObject> T find(Class<T> i_persistentClass, Long i_oid) throws DaNoPersistentClassExc, DaOidNotFoundExc {
-		if (!DaObject.class.isAssignableFrom(i_persistentClass)) {
-			throw create(DaNoPersistentClassExc.class, i_persistentClass); 
+		try {
+			this.entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnitName);
+			this.entityManager = entityManagerFactory.createEntityManager();
+		} catch (Exception ex) {
+			throw new Failure("Could not initialize JPA Entity Manager.", ex);
 		}
-		E result = em.find(i_persistentClass, i_oid);
-		if (result == null) {
-			throw create(DaOidNotFoundExc.class, i_oid);
-		}
-		return result;
-	}
-	/**
-	 * No entry found for oid "{0}" in the database
-	 */
-	@SuppressWarnings("serial")
-	public static final class DaOidNotFoundExc extends multex.Exc {}
-
-	@Override
-	public List<E> findAll(Class<E> i_persistentClass) throws DaNoPersistentClassExc {
-		if (!DaObject.class.isAssignableFrom(i_persistentClass)) {
-			throw create(DaNoPersistentClassExc.class, i_persistentClass);
-		}
-		return em.createQuery("SELECT e FROM " + i_persistentClass.getSimpleName() + "e", i_persistentClass).getResultList();
 	}
 
 	@Override
-	public List<E> findManyByQuery(Class<E> i_resultClass, String i_queryString, Object[] i_args) throws DaNoPersistentClassExc, DaArgumentCountExc {
-		if (!DaObject.class.isAssignableFrom(i_resultClass)) {
-			throw create(DaNoPersistentClassExc.class, i_resultClass); 
-		}
-		if (wrongArgumentCount(i_queryString, i_args)) {
-			throw create(DaArgumentCountExc.class, i_queryString, i_args.length);
-		}
-		String qlString = String.format(i_queryString, i_args);
-		return em.createQuery(qlString, i_resultClass).getResultList();
+	public void insert(DaObject io_object) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public void flush() {
-		em.flush();
+	public void reattach(DaObject io_object) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	public String getPersistenceUnitName() {
-		return persistenceUnitName;
+	public boolean save(DaObject io_object) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	private boolean wrongArgumentCount(String i_queryString, Object[] i_args) {
-		return countOccurences(i_queryString, "%") != i_args.length;
+	@Override
+	public boolean delete(DaObject io_object) {
+		// TODO Auto-generated method stub
+		return false;
 	}
 
-	private int countOccurences(String i_queryString, String pattern) {
-		return i_queryString.length() - i_queryString.replace(pattern, "").length();
+	@Override
+	public <E extends DaObject> E find(Class<E> persistentClass, Long oid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E extends DaObject> List<E> findAll(Class<E> persistentClass) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E extends DaObject> E findOneByKey(Class<E> persistentClass,
+			String keyFieldName, Object keyFieldValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E extends DaObject> List<E> findManyByKey(Class<E> persistentClass,
+			String keyFieldName, Object keyFieldValue) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <E extends DaObject> List<E> findManyByQuery(Class<E> resultClass,
+			Class queryClass, Object[] args) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
-	@Override
-	public List findByField(String fieldName, Object fieldValue) throws DaNoPersistentClassExc { 
-		final String OBJECT_NAME = type.getSimpleName();
-		String [] args = {
-				fieldName,
-				OBJECT_NAME,
-				(String) fieldValue
-		};
-		return pool.findManyByQuery(type, "SELECT c FROM %2$s AS c WHERE c.%1$s LIKE '%3$s'", args);
-	}
+	public EntityManager getEntityManager() {
+    	return this.entityManager;
+    }
+
 }
