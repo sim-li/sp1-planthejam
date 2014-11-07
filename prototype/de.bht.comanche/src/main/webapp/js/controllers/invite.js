edf2b937e5587da01917db4be745e2d07a5caebd
Simon Lischka <simon@lischka.co>
2014-11-07 01:29:30 +0100

    Latest - 2 - #30

--- prototype/de.bht.comanche/src/main/webapp/js/controllers/invite.js
+++ prototype/de.bht.comanche/src/main/webapp/js/controllers/invite.js
@@ -10,16 +10,44 @@ angular.module('myApp')
        {name:'Sebastian', email:'sb@gmail.com'}
     ]
 
+    $scope.groups = [
+       {name: 'Rockettes'},
+       {name: 'Ralf Laurens'},
+       {name: 'Sam Fillers'},
+       {name: 'Dam Killers'},
+       {name: 'Masaki Haki Kaki'}
+    ]
+
     $scope.addedUsers = []
-    $scope.isCollapsed = false;
+    $scope.isCollapsed = true;
+    
+    $scope.switchDetailPanel = function() {
+      if ($scope.addedUsers.length <= 0) {
+        $scope.isCollapsed = true;
+        console.log($scope.isCollapsed);
+        return;
+      }
+      $scope.isCollapsed = !$scope.isCollapsed;
+             console.log($scope.isCollapsed);
+    }
+
+    $scope.removeMember = function(index) {
+      $scope.addedUsers.splice(index, 1);
+      if ($scope.addedUsers.length <= 0) {
+        $scope.isCollapsed = true;
+      }
+    }
+
     $scope.$watch('userSelected', function() {
       for (var i = 0, len = $scope.addedUsers.length; i < len; i++) {
         if($scope.addedUsers[i] === $scope.userSelected) {
+          $scope.isCollapsed = false;
           return;
         }
       }
       if ($scope.userSelected !== undefined && $scope.userSelected.name !== undefined) {
         $scope.addedUsers.push($scope.userSelected);
+        $scope.isCollapsed = false;
       }
     });
  }]);
