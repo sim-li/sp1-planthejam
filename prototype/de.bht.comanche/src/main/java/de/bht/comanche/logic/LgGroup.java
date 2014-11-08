45094fae14147d50e7d109f07576b44bef9c71dc
Sebastian Dassé <sebdas@web.de>
2014-11-08 12:54:06 +0100

    Add missing files for groups.

---
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/logic/LgGroup.java
@@ -0,0 +1,58 @@
+package de.bht.comanche.logic;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import de.bht.comanche.persistence.DaObject;
+
+@Entity
+@Table(name = "group")
+public class LgGroup extends DaObject{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull
+	@Column
+	private String name;
+
+	@NotNull
+	@ManyToOne
+	private LgUser user;
+		
+	@OneToMany(mappedBy="group", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private List<LgMember> member;
+	
+	public String getName() {
+		return name;
+	}
+
+	public LgGroup setName(String name) {
+		this.name = name;
+		return this;
+	}
+	
+	public LgGroup save() {
+		return pool.save(this);
+	}
+	
+	public void delete() {
+		user.removeGroup(this);
+		pool.delete(this); //throw exc when delete errror
+	}
+	
+//	TODO How to find group by id - pool.find-getOid??
+//	public LgGroup getLgGroup(long oid){
+//		return null;
+//	}
+	
+
+}
---
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/logic/LgMember.java
@@ -0,0 +1,37 @@
+package de.bht.comanche.logic;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import de.bht.comanche.persistence.DaObject;
+
+@Entity
+@Table(name = "member")
+public class LgMember extends DaObject{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@NotNull
+	@ManyToOne
+	@JoinColumn(name="group_oid")
+	private LgGroup group;
+	
+	@Column
+	private long userOid;
+	
+	public long getMemberId() {
+		return userOid;
+	}
+	
+	public void setMemberId(final long userOid) {
+		this.userOid = userOid;
+	}
+	
+//	public void deleteMember(final long userOid) //only one member (it's reference from table)
+	
+}
