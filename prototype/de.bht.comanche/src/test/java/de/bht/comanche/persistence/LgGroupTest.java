69b7a82e971d9a07bc56d245aa645217d9e63487
Max <s50030@beuth-hochschule.de>
2014-11-09 17:14:09 +0100

    Fixed annotation

--- prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/LgGroupTest.java
+++ prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/LgGroupTest.java
@@ -1,36 +1,36 @@
-package de.bht.comanche.persistence;
-
-import static org.junit.Assert.assertTrue;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.persistence.Persistence;
-
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-import de.bht.comanche.logic.LgInvite;
-import de.bht.comanche.logic.LgLowLevelTransaction;
-import de.bht.comanche.logic.LgSurvey;
-import de.bht.comanche.logic.LgSurveyDummyFactory;
-import de.bht.comanche.logic.LgTransaction;
-import de.bht.comanche.logic.LgUser;
-
-public class LgGroupTest {
-	
-	@BeforeClass public static void initializeDb(){
-		Map<String, String> properties = new HashMap<String, String>(1);
-		properties.put("hibernate.hbm2ddl.auto", "create");
-		Persistence.createEntityManagerFactory(DaEmProvider.persistenceUnitName, properties);
-		assertTrue("Initialized JPA Database -> Pre Test Cleannup", true);
-	}
-	
-	@Test public void detachedTest() {
-		assertTrue("Persisting test users Alice & Bob", true);
-	}
-	
-}
+//package de.bht.comanche.persistence;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import javax.persistence.Persistence;
+//
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//import static org.hamcrest.CoreMatchers.*;
+//import de.bht.comanche.logic.LgInvite;
+//import de.bht.comanche.logic.LgLowLevelTransaction;
+//import de.bht.comanche.logic.LgSurvey;
+//import de.bht.comanche.logic.LgSurveyDummyFactory;
+//import de.bht.comanche.logic.LgTransaction;
+//import de.bht.comanche.logic.LgUser;
+//
+//public class LgGroupTest {
+//	
+//	@BeforeClass public static void initializeDb(){
+//		Map<String, String> properties = new HashMap<String, String>(1);
+//		properties.put("hibernate.hbm2ddl.auto", "create");
+//		Persistence.createEntityManagerFactory(DaEmProvider.persistenceUnitName, properties);
+//		assertTrue("Initialized JPA Database -> Pre Test Cleannup", true);
+//	}
+//	
+//	@Test public void detachedTest() {
+//		assertTrue("Persisting test users Alice & Bob", true);
+//	}
+//	
+//}
