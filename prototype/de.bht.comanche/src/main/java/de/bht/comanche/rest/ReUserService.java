401d9f2bb05826c42a13eb4b74ea015e4c0780c4
Simon Lischka <lischka@beuth-hochschule.de>
2014-11-12 12:26:02 +0100

    Correct File

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/rest/ReUserService.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/rest/ReUserService.java
@@ -12,8 +12,6 @@ import de.bht.comanche.logic.LgSession;
 import de.bht.comanche.logic.LgTransaction;
 import de.bht.comanche.logic.LgUser;
 
-//Some text added
-
 @Path("/user/")
 public class ReUserService extends RestService {
 	final String USER_OID = "userOid";
