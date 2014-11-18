4788bd1a15274b6cc02503a53d3dda606cfeb194
Sebastian Dassé <sebdas@web.de>
2014-11-18 11:42:49 +0100

    Add missing file.

---
+++ prototype/de.bht.comanche/src/main/webapp/test/unit/restSpec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* jasmine specs for the rest services go here */
+describe('restModule', function() {
+
+    beforeEach(module('restModule'));
+
+    it('should should exist', inject(function(restService) {
+        expect(restService).toBeDefined();
+    }));
+
+});
