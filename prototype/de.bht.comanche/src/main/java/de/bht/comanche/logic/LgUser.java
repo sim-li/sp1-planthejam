2247efaf8a7d0c9e6db550245ba2e295524f65ba
Max <s50030@beuth-hochschule.de>
2014-12-03 10:00:38 +0100

    Add part of docs in LgUser

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/logic/LgUser.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/logic/LgUser.java
@@ -11,46 +11,80 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import de.bht.comanche.persistence.DaObject;
-
+/**
+ * This entity class represents a user and serve methods for working with 
+ * all objects LgClasses.
+ * 
+ * @author Simon Lischka
+ *
+ */
 @Entity
 @Table(name = "user", uniqueConstraints=@UniqueConstraint(columnNames="NAME"))
 public class LgUser extends DaObject {
 	
 	private static final long serialVersionUID = 1L;
-	
+	/**
+	 * Column for a user name. Must not be null.
+	 */
 	@Column(unique=true, nullable=false)
 	private String name;
+	/**
+	 * Column for a user's telephone.
+	 */
 	private String tel;
+	/**
+	 * Column for a user's email.
+	 */
 	private String email;
+	/**
+	 * Column for a user's password.
+	 */
 	private String password;
-
+	/**
+	 * Representation of a foreign key in a LgInvite entity. Provide a list of invites. 
+	 */
 	@OneToMany(mappedBy="user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
 	private List<LgInvite> invites;
-	
+	/**
+	 * Representation of a foreign key in a LgGroup entity. Provide a list of groups. 
+	 */
 	@OneToMany(mappedBy="user", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER, orphanRemoval=true)
 	private List<LgGroup> groups;
-	
+	/**
+	 * Representation of a foreign key in a LgMember entity. Provide a member. 
+	 */
 	@OneToOne(mappedBy="user", cascade = CascadeType.REMOVE, fetch = FetchType.EAGER, orphanRemoval=true)
 	private LgMember member;
 	
+	/**
+	 * Construct a new LgUser object with a list of groups and members.
+	 */
 	public LgUser() {
 		this.invites = new ArrayList<LgInvite>();
 		this.groups = new ArrayList<LgGroup>();
 	}
 	
+	/**
+	 * Save Invite for current user.
+	 * @param invite The LgInvite to save.
+	 * @return The saved LgInvite.
+	 */
 	public LgInvite save(final LgInvite invite) {
         invite.setUser(this);
 		return attach(invite).save();
 	}
-	
+	/**
+	 * Complete deleting of a user accout.
+	 */
 	public void deleteAccount() {
 		delete();
 	}
-
+	/**
+	 * Delete LgInvite by provided oid.
+	 * @param inviteOid The LgInvite oid.
+	 */
 	public void deleteInvite(final long inviteOid) {
 		getInvite(inviteOid).delete();
 	}
