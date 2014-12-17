ea7b860616d7944b610087017cd86d989e532077
Max <s50030@beuth-hochschule.de>
2014-12-17 17:54:01 +0100

    Changed ReInviteService

--- prototype/de.bht.comanche/src/main/java/de/bht/comanche/rest/ReInviteService.java
+++ prototype/de.bht.comanche/src/main/java/de/bht/comanche/rest/ReInviteService.java
@@ -7,8 +7,10 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 
@@ -31,8 +33,8 @@ public class ReInviteService extends RestService {
 	 * @return The list of invites.
 	 * @exception RestGetInvitesFailure if it was not possible to get list of invites for current user.
 	 */
-	@POST
-	@Path("getInvites")
+	@GET
+	@Path("get")
 	@Consumes("application/json")
 	@Produces({ "application/json" })
 	public List<LgInvite> get(@Context final HttpServletRequest request) {
@@ -41,7 +43,7 @@ public class ReInviteService extends RestService {
 			public List<LgInvite> execute() throws Exception {
 				final List<LgInvite> result;
 				try {
-					result = startSession().getInvites();
+					result = startSession().getInvites();//TODO change the method
 				} catch (Exception ex) {
 					throw create(RestGetInvitesFailure.class, ex, getSession().getUser().getName());
 				}
@@ -63,17 +65,17 @@ public class ReInviteService extends RestService {
 	 * @return The LgInvite.
 	 * @exception RestGetInviteFailure if it was not possible to get invite for current user.
 	 */
-	@POST
-	@Path("get")
+	@GET
+	@Path("get/{oid}")
 	@Consumes("application/json")
 	@Produces({ "application/json" })
-	public LgInvite get(final long oid, @Context final HttpServletRequest request) {
+	public LgInvite get(@PathParam("oid") final long oid, @Context final HttpServletRequest request) {
 		return new LgTransaction<LgInvite>(request) {
 			@Override
 			public LgInvite execute() throws Exception {
 				final LgInvite result;
 				try {
-					result = startSession().getInvite(oid);
+					result = startSession().getInvite(oid);//TODO change the method
 				} catch (Exception ex) {
 					throw create(RestGetInviteFailure.class, ex, oid, getSession().getUser().getName());
 				}
@@ -103,7 +105,7 @@ public class ReInviteService extends RestService {
 			public LgInvite execute() throws Exception {
 				final LgInvite result;
 				try {
-					result = startSession().save(invite);
+					result = startSession().save(invite);//TODO change the method
 				} catch (Exception ex) {
 					throw create(RestSaveInviteFailure.class, ex, invite.getOid(), getSession().getUser().getName());
 				}
@@ -118,67 +120,67 @@ public class ReInviteService extends RestService {
 	@SuppressWarnings("serial")
 	public static final class RestSaveInviteFailure extends multex.Failure {}
 
-	/**
-	 * Delete LgInvite by specified oid.
-	 * @param oid The LgInvite oid.
-	 * @param request The request information from HTTP service.
-	 * @exception RestDeleteInviteFailure if it was not possible to delete invite of current user.
-	 */
-	@Path("delete")
-	@DELETE
-	@Consumes("application/json")
-	@Produces({ "application/json" })
-	public LgInvite delete(final long oid, @Context final HttpServletRequest request) {
-		return new LgTransaction <LgInvite>(request) {
-			public LgInvite execute() throws Exception {
-				try {
-					startSession().deleteInvite(oid);
-				} catch (Exception ex) {
-					throw create(RestDeleteInviteFailure.class, ex, oid, getSession().getUser().getName());
-				}
-				return null;
-			}
-		}.getResult();
-	}
-
-	/**
-	 * Could not delete invite with oid "{0}" for user "{1}"
-	 */
-	@SuppressWarnings("serial")
-	public static final class RestDeleteInviteFailure extends multex.Failure {}
-	
-	/**
-	 * Returns a list of all invites that belong to the specified invite's survey.
-	 * 
-	 * @param oid The invite oid.
-	 * @param request The request information from HTTP service.
-	 * @return The list of invites.
-	 * @exception RestGetInvitesFailure if it was not possible to get list of invites for current user.
-	 */
-	@POST
-	@Path("getSurveyInvites")
-	@Consumes("application/json")
-	@Produces({ "application/json" })
-	public List<LgInvite> getSurveyInvites(final long oid, @Context final HttpServletRequest request) {
-		return new LgTransaction<List<LgInvite>>(request) {
-			@Override
-			public List<LgInvite> execute() throws Exception {
-				final List<LgInvite> result;
-				try {
-					result = startSession().getInvite(oid).getSurvey().getInvites();
-					System.err.printf("############### number of invites for survey #%d = %d%n", oid, result.size());
-				} catch (Exception ex) {
-					throw create(RestGetSurveyInvitesFailure.class, ex, oid, getSession().getUser().getName());
-				}
-				return result;
-			}
-		}.getResult();
-	}
-
-	/**
-	 * Could not get invites of survey of invite with oid "{0}" for user "{1}"
-	 */
-	@SuppressWarnings("serial")
-	public static final class RestGetSurveyInvitesFailure extends multex.Failure {}
+//	/**
+//	 * Delete LgInvite by specified oid.
+//	 * @param oid The LgInvite oid.
+//	 * @param request The request information from HTTP service.
+//	 * @exception RestDeleteInviteFailure if it was not possible to delete invite of current user.
+//	 */
+//	@Path("delete")
+//	@DELETE
+//	@Consumes("application/json")
+//	@Produces({ "application/json" })
+//	public LgInvite delete(final long oid, @Context final HttpServletRequest request) {
+//		return new LgTransaction <LgInvite>(request) {
+//			public LgInvite execute() throws Exception {
+//				try {
+//					startSession().deleteInvite(oid);
+//				} catch (Exception ex) {
+//					throw create(RestDeleteInviteFailure.class, ex, oid, getSession().getUser().getName());
+//				}
+//				return null;
+//			}
+//		}.getResult();
+//	}
+//
+//	/**
+//	 * Could not delete invite with oid "{0}" for user "{1}"
+//	 */
+//	@SuppressWarnings("serial")
+//	public static final class RestDeleteInviteFailure extends multex.Failure {}
+//	
+//	/**
+//	 * Returns a list of all invites that belong to the specified invite's survey.
+//	 * 
+//	 * @param oid The invite oid.
+//	 * @param request The request information from HTTP service.
+//	 * @return The list of invites.
+//	 * @exception RestGetInvitesFailure if it was not possible to get list of invites for current user.
+//	 */
+//	@POST
+//	@Path("getSurveyInvites")
+//	@Consumes("application/json")
+//	@Produces({ "application/json" })
+//	public List<LgInvite> getSurveyInvites(final long oid, @Context final HttpServletRequest request) {
+//		return new LgTransaction<List<LgInvite>>(request) {
+//			@Override
+//			public List<LgInvite> execute() throws Exception {
+//				final List<LgInvite> result;
+//				try {
+//					result = startSession().getInvite(oid).getSurvey().getInvites();
+//					System.err.printf("############### number of invites for survey #%d = %d%n", oid, result.size());
+//				} catch (Exception ex) {
+//					throw create(RestGetSurveyInvitesFailure.class, ex, oid, getSession().getUser().getName());
+//				}
+//				return result;
+//			}
+//		}.getResult();
+//	}
+//
+//	/**
+//	 * Could not get invites of survey of invite with oid "{0}" for user "{1}"
+//	 */
+//	@SuppressWarnings("serial")
+//	public static final class RestGetSurveyInvitesFailure extends multex.Failure {}
 
 }
