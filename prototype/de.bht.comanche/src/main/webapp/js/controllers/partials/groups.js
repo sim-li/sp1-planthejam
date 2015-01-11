67dabc21e3280a8b74ef062fd1457fc95aee2e34
Simon Lischka <simon@lischka.co>
2015-01-11 18:32:34 +0100

    Prepare state switching -2- #61

--- prototype/de.bht.comanche/src/main/webapp/js/controllers/partials/groups.js
+++ prototype/de.bht.comanche/src/main/webapp/js/controllers/partials/groups.js
@@ -2,171 +2,141 @@
 
 angular.module('myApp')
     .controller('groupsCtrl', ['$scope', '$modal', '$log', 'restService', 'StateSwitcher',
-        function($scope, $modal, $log, restService, StateSwitcher) {
+            function($scope, $modal, $log, restService, StateSwitcher) {
 
-            /**
-             * Retrieves users and groups form promise of
-             * parent controller (using current $scope)
-             */
-            var users, groups;
-            (function resolvePromises() {
-                users = $scope.users;
-                groups = $scope.groups;
-            })(users, groups);
 
-            /**
-             * Create simple variables needed to store UI states for components
-             * that aren't grouped in directives.
-             */
-            (function createSimpleUIStateVariables() {
-                $scope.panelOpened = false;
-            })();
+                /**
+                 * Creates a new modal instance and opens it.
+                 * TODO: Improve doc
+                 * @param  {[type]} size [description]
+                 * @return {[type]}      [description]
+                 */
+                $scope.openGroupModal = function() {
+                    var modalInstance = $modal.open({
+                        templateUrl: 'groupsModalContent.html',
+                        controller: 'groupsModalCtrl',
+                        size: 'lg',
+                        resolve: {
+                            groups: function() {
+                                console.log(groups);
+                                return groups;
+                            },
+                            users: function() {
+                                return users;
+                            }
+                        }
+                    });
+                    modalInstance.result.then(function(selectedItem) {
+                        $scope.selected = selectedItem;
+                    }, function() {
+                        $log.info('Modal dismissed at: ' + new Date());
+                    });
+                };
 
-            (function createSimpleModelVariables() {
-                $scope.lastElementSelected = '';
-                $scope.allElementsSelected = [];
-            })();
+                /**
+                 * Retrieves users and groups form promise of
+                 * parent controller (using current $scope)
+                 */
+                var users, groups;
+                (function resolvePromises() {
+                    users = $scope.users;
+                    groups = $scope.groups;
+                })(users, groups);
 
-            var panelOpener = new StateSwitcher({
-                switchOnAction: function() {
-                    addSelectionToModel(modelReference);
-                    $scope.panelOpened = true;
-                    console.log('Got called');
-
-                },
-                switchOffAction: function() {
+                /**
+                 * Create simple variables needed to store UI states for components
+                 * that aren't grouped in directives.
+                 */
+                (function createSimpleUIStateVariables() {
                     $scope.panelOpened = false;
-                },
-                condition: function() {
-                    return checkIfValidSelection() && checkForDuplicates();
+                })();
+
+                (function createSimpleModelVariables() {
+                    $scope.lastElementSelected = '';
+                    $scope.allElementsSelected = [];
+                })();
+
+                $scope.$watch('allElementsSelected', function() {
+                    panelOpener.toggle();
+                });
+
+                var panelOpener = new StateSwitcher({
+                    switchOnAction: function() {
+                        addCurrentElementToSelection();
+                        $scope.panelOpened = true;
+                        console.log('Got called');
+
+                    },
+                    switchOffAction: function() {
+                        $scope.panelOpened = false;
+                    },
+                    condition: function() {
+                        return isValidSelection() && isNoDuplicate();
+                    }
+                });
+
+                var isValidSelection = function() {
+                    if (!($scope.lastElementSelected && $scope.lastElementSelected.name)) {
+                        return false;
+                    }
+                    return true;
                 }
-            });
 
-            $scope.$watch('userSelected', function() {
-                panelOpener.toggle();
-            });
-
-
-            var checkIfValidSelection = function() {
-                if (!($scope.userSelected && $scope.userSelected.name)) {
-                    return;
-                }
-            }
-
-            var checkForDuplicates = function() {
+                var isNoDuplicate = function() {
                     for (var i = 0, len = $scope.addedUsers.length; i < len; i++) {
                         if ($scope.addedUsers[i] === $scope.userSelected) {
-                            $scope.isCollapsed = false;
-                            return;
+                            return false;
                         }
                     }
+                    return true;
                 }
-                // These two have dependencies
-                //
-            var addSelectionToModel = function(modelReference) {
-                $scope.addedUsers.push($scope.userSelected);
-            }
+
+                var addCurrentElementToSelection = function() {
+                    $scope.allElementsSelected.push(lastElementSelected);
+                }
+
+                $scope.removeElementFromSelection = function(index) {
+                    $scope.allElementsSelected.splice(index, 1);
+                }
+            ]);
+
+        // Please note that $modalInstance represents a modal window (instance) dependency.
+        // It is not the same as the $modal service used above.
+
+        angular.module('myApp')
+        .controller('groupsModalCtrl', function($scope, $modalInstance, groups, users) {
+            $scope.test = 'My test!';
+            $scope.groups = groups;
+            $scope.users = users;
+            console.log('GRAV URL', groups);
+            $scope.selectedGroup = '';
+            $scope.userSelected = '';
 
             /**
-             * Creates a new modal instance and opens it.
-             * TODO: Improve doc
-             * @param  {[type]} size [description]
-             * @return {[type]}      [description]
+             * This must be transferred into a generic solution, simply not convincing.
+             * Must dock to actual data model.
+             * @return {[type]} [description]
              */
-            $scope.openGroupModal = function() {
-                var modalInstance = $modal.open({
-                    templateUrl: 'groupsModalContent.html',
-                    controller: 'groupsModalCtrl',
-                    size: 'lg',
-                    resolve: {
-                        groups: function() {
-                            console.log(groups);
-                            return groups;
-                        },
-                        users: function() {
-                            return users;
-                        }
-                    }
-                });
-                modalInstance.result.then(function(selectedItem) {
-                    $scope.selected = selectedItem;
-                }, function() {
-                    $log.info('Modal dismissed at: ' + new Date());
-                });
-            };
-
-
-
-            // $scope.$watch('userSelected', function() {
-            //     checkIfValidSelection();
-            //     checkForDuplicates();
-            //     // addSelectionToModel(modelReference);
-            //     collapseSwitcher.on();
-            // });
-
-
-            // $scope.removeMember = function(index) {
-            //     $scope.addedUsers.splice(index, 1);
-            //     if ($scope.addedUsers.length <= 0) {
-            //         $scope.isCollapsed = true;
-            //     }
-            // };
-
-            // $scope.selectGroup = function(groupName) {
-            //     if (groupName === undefined || $scope.groups === undefined) {
-            //         return;
-            //     }
-            //     var group = getGroup(groupName);
-            //     if (group === -1) {
-            //         return;
-            //     }
-            //     $scope.selectedGroupName = group.name;
-            //     $scope.editedGroupName = group.name;
-            //     $scope.addedUsers = [];
-            //     $scope.addedUsers = group.members;
-            //     $scope.showTrash = true;
-            //     // $scope.openDetailPanel();
-            // };
-        }
-    ]);
-
-// Please note that $modalInstance represents a modal window (instance) dependency.
-// It is not the same as the $modal service used above.
-
-angular.module('myApp')
-    .controller('groupsModalCtrl', function($scope, $modalInstance, groups, users) {
-        $scope.test = 'My test!';
-        $scope.groups = groups;
-        $scope.users = users;
-        console.log('GRAV URL', groups);
-        $scope.selectedGroup = '';
-        $scope.userSelected = '';
-
-        /**
-         * This must be transferred into a generic solution, simply not convincing.
-         * Must dock to actual data model.
-         * @return {[type]} [description]
-         */
-        $scope.$watch('userSelected', function() {
-            if ($scope.userSelected === undefined || $scope.userSelected.name === undefined) {
-                return;
-            }
-            for (var i = 0, len = $scope.addedUsers.length; i < len; i++) {
-                if ($scope.addedUsers[i] === $scope.userSelected) {
+            $scope.$watch('userSelected', function() {
+                if ($scope.userSelected === undefined || $scope.userSelected.name === undefined) {
                     return;
                 }
-            }
-            $scope.addedUsers.push($scope.userSelected);
+                for (var i = 0, len = $scope.addedUsers.length; i < len; i++) {
+                    if ($scope.addedUsers[i] === $scope.userSelected) {
+                        return;
+                    }
+                }
+                $scope.addedUsers.push($scope.userSelected);
+            });
+
+            $scope.selectGroup = function(group) {
+                $scope.selectedGroup = group;
+            };
+
+            $scope.ok = function() {
+                // $modalInstance.close($scope.selected.item);
+            };
+            $scope.cancel = function() {
+                $modalInstance.dismiss('cancel');
+            };
         });
-
-        $scope.selectGroup = function(group) {
-            $scope.selectedGroup = group;
-        };
-
-        $scope.ok = function() {
-            // $modalInstance.close($scope.selected.item);
-        };
-        $scope.cancel = function() {
-            $modalInstance.dismiss('cancel');
-        };
-    });
