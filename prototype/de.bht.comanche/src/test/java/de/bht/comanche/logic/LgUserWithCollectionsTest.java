1e38d7e29bdb47551b275abde93c26c28056a88f
Simon Lischka <simon@lischka.co>
2015-01-19 19:19:35 +0100

    Test udpates

--- prototype/de.bht.comanche/src/test/java/de/bht/comanche/logic/LgUserWithCollectionsTest.java
+++ prototype/de.bht.comanche/src/test/java/de/bht/comanche/logic/LgUserWithCollectionsTest.java
@@ -27,76 +27,97 @@ import de.bht.comanche.persistence.DaEmProvider;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LgUserWithCollectionsTest {
-	private LgUser alice;
-	private LgSession session;
-	private LgTimePeriod timePeriod;
-	
-	@BeforeClass 
-	public static void resetDatabase() {
-		Map<String, String> properties = new HashMap<String, String>(1);
-		properties.put("hibernate.hbm2ddl.auto", "create");
-		Persistence.createEntityManagerFactory(DaEmProvider.PERSISTENCE_UNIT_NAME, properties);
-	}
-	
-	@Before
-	public void buildUp() {
-		beginTransaction();
-		intializeUsers();
-		registerUsers(session);
-		alice = session.startFor("Alice");
-		timePeriod = new LgTimePeriod()
-			.setDurationMins(10)
-			.setStartTime(new Date(8099));
-	}
+    private LgUser alice;
+    private LgSession session;
+    private LgTimePeriod timePeriod;
 
-	private void registerUsers(final LgSession sessionForRegistration) {
-		sessionForRegistration.register(alice);
-	}
+    @BeforeClass
+    public static void resetDatabase() {
+        Map<String, String> properties = new HashMap<String, String>(1);
+        properties.put("hibernate.hbm2ddl.auto", "create");
+        Persistence.createEntityManagerFactory(DaEmProvider.PERSISTENCE_UNIT_NAME, properties);
+    }
 
-	private void intializeUsers() {
-		alice = new LgUser().setName("Alice").setEmail("test@test.de")
-				.setPassword("testtest");
-	}
-	
-	/**
-	 * This test actually includes the checking of data, thus the part before the commit 
-	 * is what would be called in a Rest Service. 
-	 */
-	@Test
-	public void saveMessagesTest() {
+    @Before
+    public void buildUp() {
+        beginTransaction();
+        intializeUsers();
+        registerUsers(session);
+        alice = session.startFor("Alice");
+        timePeriod = new LgTimePeriod()
+            .setDurationMins(10)
+            .setStartTime(new Date(8099));
+    }
+
+    private void registerUsers(final LgSession sessionForRegistration) {
+        sessionForRegistration.register(alice);
+    }
+
+    private void intializeUsers() {
+        alice = new LgUser().setName("Alice").setEmail("test@test.de")
+                .setPassword("testtest");
+    }
+
+    /**
+     * This test actually includes the checking of data, thus the part before the commit
+     * is what would be called in a Rest Service.
+     */
+    @Test
+    public void saveMessagesTest() {
+        saveDemoMessages();
+        commit();
+        alice = session.startFor("Alice");
+        assertTrue("Alice has message 'Hello'", alice.getMessages().contains("Hello"));
+        assertTrue("Alice has message 'Kitty'", alice.getMessages().contains("Kitty"));
+    }
+    
+    @Test
+    public void removeAndAddUpdateMessageTest() {
+    	 saveDemoMessages();
+    	 commit();
+    	 alice = session.startFor("Alice");
+    	 alice.getMessages().remove("Hello");
+    	 alice.getMessages().add("Update");
+    	 commit();
+    	 alice = session.startFor("Alice");
+    	 assertTrue("Alice has Updated message", alice.getMessages().contains("Update"));
+    	 assertFalse("Alice does not have old message", alice.getMessages().contains("Hello"));
+    	 for (String msg : alice.getMessages()) {
+    		 System.out.println("MSG:>" + msg);
+    	 }
+    }
+
+	private void saveDemoMessages() {
 		List<String> messages = new ArrayList<String>();
-		messages.add("Hello");
-		messages.add("Kitty");
-		alice.setMessages(messages);
-		alice.save();
-		commit();
-		final LgUser alicePersisted = session.startFor("Alice");
-		assertTrue(alicePersisted.getMessages().contains("Hello"));
-		assertTrue(alicePersisted.getMessages().contains("Kitty"));
+        messages.add("Hello");
+        messages.add("Kitty");
+        alice.setMessages(messages);
+        alice.save();
 	}
-	
-	@Ignore
-	@Test
-	public void saveTimePeriodTest() {
-	}
-	
-	@After
-	public void tearDown() {
-		alice.deleteThisAccount();
-//		endTransaction();
-	}
-				
-	private void beginTransaction() {
-		session = new LgSession();
-		session.getApplication().beginTransaction();
-	}
-	
-	private void endTransaction() {
-		session.getApplication().endTransaction(true);
-	}
-	
-	private void commit() {
-		beginTransaction();
-		endTransaction();
-	}
+
+    
+    @Ignore
+    @Test
+    public void saveTimePeriodTest() {
+    }
+
+    @After
+    public void tearDown() {
+        alice.deleteThisAccount();
+        endTransaction();
+    }
+
+    private void beginTransaction() {
+        session = new LgSession();
+        session.getApplication().beginTransaction();
+    }
+
+    private void endTransaction() {
+        session.getApplication().endTransaction(true);
+    }
+
+    private void commit() {
+        endTransaction();
+        beginTransaction();
+    }
 }
