0255ab71c6fcb3a73a7c8c26b0eaf5aa4e3ec7af
Sebastian Dassé <sebdas@web.de>
2015-01-23 17:45:42 +0100

    Still debugging calendar modal.

--- prototype/de.bht.comanche/src/main/webapp/js/controllers/partials/calendar.js
+++ prototype/de.bht.comanche/src/main/webapp/js/controllers/partials/calendar.js
@@ -5,27 +5,14 @@ angular.module('myApp')
 
             'use strict';
 
-            $scope.eventSources = [];
-
             $scope.possibleTimePeriods = $scope.possibleTimePeriods || [];
             // $scope.resultingTimePeriods = $scope.resultingTimePeriods || [];
 
-            //-- dummies -->
-            // $scope.possibleTimePeriods.push({
-            //     startTime: new Date('Jan 23 2015 10:00'),
-            //     durationMins: 120
-            // });
-            // $scope.possibleTimePeriods.push({
-            //     startTime: new Date('Jan 24 2015 6:00'),
-            //     durationMins: 120
-            // });
-            // <-----------
-
-            // placeholder all events for rendering ui-calendar
-            $scope.uiTimePeriods = [];
-            // copy all elements of possibleTimePeriods to uiTimePeriods and convert durationMins to end
+            // placeholder for all events for rendering ui-calendar
+            $scope.eventSources = [];
+            // copy all elements of possibleTimePeriods to eventSources and convert durationMins to end
             arrayUtil.forEach($scope.possibleTimePeriods, function(timePeriod) {
-                $scope.uiTimePeriods.push({
+                $scope.eventSources.push({
                     id: 'possible',
                     rendering: 'background',
                     // start: timePeriod.startTime,
@@ -53,79 +40,42 @@ angular.module('myApp')
                         // var start = new Date(startDate + timeZoneOffset * 60000);
                         // var end = new Date(endDate + timeZoneOffset * 60000);
 
-                        $scope.uiTimePeriods.push({
-                            // start: start,
-                            // end: end,
+                        $scope.eventSources.push({
                             // start: startDate.zone(timeZoneOffset),
                             // end: endDate.zone(timeZoneOffset),
-                    start: startDate,
-                    end: endDate,
+                            start: startDate,
+                            end: endDate,
                             editable: true,
                             durationEditable: true,
                             constraint: 'possible'
                         });
 
-                        $scope.resultingTimePeriods.push(new TimePeriod({
-                            startTime: startDate.toDate(),
-                            durationMins: endDate.diff(startDate, 'minutes')
-                        }));
+                        // $scope.resultingTimePeriods.push(new TimePeriod({
+                        //     startTime: startDate.toDate(),
+                        //     durationMins: endDate.diff(startDate, 'minutes')
+                        // }));
 
                         $log.debug('----------')
                         $log.debug('- debug calendar -')
                             // $log.debug($scope.test.survey.name)
-                            // $log.debug($scope.eventSources)
-                        $log.debug($scope.possibleTimePeriods)
-                        $log.debug($scope.uiTimePeriods)
-                        $log.debug($scope.resultingTimePeriods)
+                        $log.debug('possible : ', $scope.possibleTimePeriods)
+                        $log.debug('ui       : ', $scope.eventSources)
+                        $log.debug('resulting: ', $scope.resultingTimePeriods)
                         $log.debug('----------')
 
                     },
                     eventResize: function(event, delta, reverFunc, jsEvent, ui, view) {
-                        console.log(delta)
-                        var ele = arrayUtil.findByAttribute($scope.uiTimePeriods, 'id', event.id);
-                        console.log(ele.start.toDate())
-                        console.log(ele.end.toDate())
-                        ele.end = ele.end.add(delta);
-                        console.log(ele.start.toDate())
-                        console.log(ele.end.toDate())
-                        console.log(event)
+                        var ele = arrayUtil.findByAttribute($scope.eventSources, 'id', event.id);
+                        ele.end = event.end;
                     },
                     eventDrop: function(event, delta, revertFunc, jsEvent, ui, view) {
-                        // console.log(delta)
-                        // console.log(delta.asMinutes())
-                        // console.log(event)
-                        var ele = arrayUtil.findByAttribute($scope.uiTimePeriods, 'id', event.id);
-                        // ele.start = event.start,
-                        // ele.end = event.end;
-                        /*console.log(event.start.toDate())
-                        console.log(ele.start)*/
-                        console.log(ele)
-                        console.log(event)
-                        console.log(ele.start.toDate())
-                        console.log(ele.end.toDate())
-                        ele.start = ele.start.add(delta);
-                        ele.end = ele.end.add(delta);
-                        console.log(ele.start.toDate())
-                        console.log(ele.end.toDate())
-
-                        // console.log(event.start.add(delta.asMinutes(), 'minutes').toDate())
-                        // ele = {
-                        //     start: event.start
-                        // }
-                        // console.log(ele)
-                        // console.log($scope.uiTimePeriods)
-
-                        // $scope.uiTimePeriods.push({
-                        //     start: start,
-                        //     end: end,
-                        //     editable: true,
-                        //     durationEditable: true,
-                        //     constraint: 'possible'
-                        // });
+                        var ele = arrayUtil.findByAttribute($scope.eventSources, 'id', event.id);
+                        ele.start = event.start;
+                        ele.end = event.end;
                     },
-                    selectConstraint: $scope.uiTimePeriods.length > 0 ? 'possible' : null,
+                    selectConstraint: $scope.eventSources.length > 0 ? 'possible' : null,
                     // selectConstraint: 'possible',
-                    events: $scope.uiTimePeriods
+                    events: $scope.eventSources
                 }
 
             };
