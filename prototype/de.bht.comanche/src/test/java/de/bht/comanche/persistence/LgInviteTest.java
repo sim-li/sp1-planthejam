fbcedeff0cc44c9ccebf38ee38a2170d96ee61f0
Simon Lischka <simon@lischka.co>
2015-01-26 03:43:02 +0100

    Refactor invite test

--- prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/LgInviteTest.java
+++ prototype/de.bht.comanche/src/test/java/de/bht/comanche/persistence/LgInviteTest.java
@@ -22,116 +22,138 @@ import de.bht.comanche.rest.ReErrorMessage;
 import de.bht.comanche.rest.ReServerException;
 
 public class LgInviteTest {
+	private TestUtils testUtils;
 	private LgUser alice;
 	private LgUser bob;
 	private LgUser carol;
 	private LgUser denise;
-    private LgUser edgar;
-    private List<LgInvite> invites = new LinkedList<LgInvite>();
-	private TestUtils testUtils = new TestUtils();
-	private LgInvite bobsFirstInvite;
-
+	private LgUser edgar;
+	private LgSurvey sportingSurvey;
+	private LgSurvey swimSurvey;
+	private LgSurvey denimSurvey;
+	private LgInvite sportingSurveyInvite;
+	
 	@BeforeClass
 	public static void init() {
 		TestUtils.resetJPADatabase();
 	}
-	
+
 	@Before
 	public void buildUp() {
-		alice = testUtils.registerTestUser("Alice");
+		testUtils = new TestUtils();
+		registerUsers();
+		saveSurveys();
+		sportingSurveyInvite = sportingSurvey.getInviteByParticipantName("Bob");
+	}
+
+	private void registerUsers() {
 		bob = testUtils.registerTestUser("Bob");
+		alice = testUtils.registerTestUser("Alice");
 		carol = testUtils.registerTestUser("Carol");
 		denise = testUtils.registerTestUser("Denise");
 		edgar = testUtils.registerTestUser("Edgar");
-		saveInvitesFor(bob); 
 	}
 
-	private void saveInvitesFor(LgUser user) {
-		final LgSurvey survey1 = testUtils.saveSurveyFor("Alice", new LgSurvey()
-		.addParticipants(user, carol, denise, edgar)
-		.setName("Sporting Event"));
-		final LgSurvey survey2 = testUtils.saveSurveyFor("Carol", new LgSurvey()
-		.addParticipants(user, alice, denise, edgar)
-		.setName("Swim Course"));
-		final LgSurvey survey3 = testUtils.saveSurveyFor("Denise", new LgSurvey()
-		.addParticipants(alice, carol, edgar)
-		.setName("Lou Reed Music Club"));
-		final LgSurvey survey4 = testUtils.saveSurveyFor("Edgar", new LgSurvey()
-		.addParticipants(user, alice, carol, denise)
-		.setName("Denim Jeans Reunion"));
-		invites.add(survey1.getInviteByParticipantName("Bob"));
-		invites.add(survey2.getInviteByParticipantName("Bob"));
-		invites.add(survey4.getInviteByParticipantName("Bob"));
-		bobsFirstInvite = survey1.getInviteByParticipantName("Bob");
+	/**
+	 * Saves various test surveys which include Bob as participant. He is not
+	 * invited to Lou Reed Music Club. This way we can check that he really
+	 * receives only the invites he's a participant in.
+	 */
+	private void saveSurveys() {
+		sportingSurvey = testUtils.saveSurveyFor("Alice",
+				new LgSurvey().addParticipants(bob, carol, denise, edgar)
+						.setName("Sporting Event"));
+		swimSurvey = testUtils.saveSurveyFor("Carol",
+				new LgSurvey().addParticipants(bob, alice, denise, edgar)
+						.setName("Swim Course"));
+		denimSurvey = testUtils.saveSurveyFor("Edgar",
+				new LgSurvey().addParticipants(bob, alice, carol, denise)
+						.setName("Denim Jeans Reunion"));
+		testUtils.saveSurveyFor(
+				"Denise",
+				new LgSurvey().addParticipants(alice, carol, edgar).setName(
+						"Lou Reed Music Club"));
 	}
-	
+
 	@Test
 	public void getInviteByOidUserNameTest() {
 		final List<LgInvite> invitesForEvaluation = getAllInvitesByOid();
-		assertThat(
-				extractProperty("user.name").from(
-						invitesForEvaluation)).containsOnly(
-								"Bob", "Bob", "Bob");
+		assertThat(extractProperty("user.name").from(invitesForEvaluation))
+				.containsOnly("Bob", "Bob", "Bob");
 	}
+
 	@Test
 	public void getInviteByOidSurveyNameTest() {
 		final List<LgInvite> invitesForEvaluation = getAllInvitesByOid();
-		assertThat(
-			extractProperty("survey.name").from(
-					invitesForEvaluation)).containsOnly(
-							"Sporting Event", "Swim Course", "Denim Jeans Reunion");
+		assertThat(extractProperty("survey.name").from(invitesForEvaluation))
+				.containsOnly("Sporting Event", "Swim Course",
+						"Denim Jeans Reunion");
 	}
-	
+
+	/**
+	 * Executes getInvite([ oid ]) on all surveys. We don't check on the Lou
+	 * Reed Survey (throw null pointer exception, should be corrected in other
+	 * moment)
+	 * 
+	 * @return Invites fetched from surveys.
+	 */
 	private List<LgInvite> getAllInvitesByOid() {
-		final List<LgInvite> invitesForEvaluation = new TestTransaction<List<LgInvite>>("Bob") {
+		final List<LgInvite> invitesForEvaluation = new TestTransaction<List<LgInvite>>(
+				"Bob") {
 			@Override
 			public List<LgInvite> execute() {
-				final List <LgInvite> invitesFetch = new ArrayList<LgInvite>();
-				final LgUser bob = startSession();
-				for (LgInvite inv : invites) {
-					invitesFetch.add(bob.getInvite(inv.getOid()));
+				final List<LgInvite> invitesFetch = new ArrayList<LgInvite>();
+				for (LgInvite invite : getInvitesFromSurveys(sportingSurvey,
+						swimSurvey, denimSurvey)) {
+					invitesFetch.add(startSession().getInvite(invite.getOid()));
 				}
 				return invitesFetch;
 			}
 		}.getResult();
 		return invitesForEvaluation;
 	}
-	
+
 	/**
-	 * Note: Surveys will be transmitted with invites, we are checking for invites form certain perspective
+	 * Extracts the invites for Bob from the given surveys.
+	 * 
+	 * @param Surveys
+	 *            to be queried
+	 * @return Invites for Bob
 	 */
-	
+	private List<LgInvite> getInvitesFromSurveys(LgSurvey... surveys) {
+		final List<LgInvite> invites = new LinkedList<LgInvite>();
+		for (LgSurvey survey : surveys) {
+			invites.add(survey.getInviteByParticipantName("Bob"));
+		}
+		return invites;
+	}
+
 	@Test
 	public void getInvitesAsParticipantIsIgnoredFlagTest() {
 		final List<LgInvite> invites = getInvitesAsParticipantFor("Bob");
-		assertThat(
-				extractProperty("isIgnored").from(
-						invites)).containsOnly(
-								LgStatus.UNDECIDED);
+		assertThat(extractProperty("isIgnored").from(invites)).containsOnly(
+				LgStatus.UNDECIDED);
 	}
-	
+
 	@Test
 	public void getInvitesAsParticipantIsHostFlagTest() {
 		final List<LgInvite> invites = getInvitesAsParticipantFor("Bob");
 		// Check that only surveys ment for Bob are retrieved.
 		// Bob should not receive Lou Reed Music Club!
-		assertThat(
-				extractProperty("isHost").from(
-						invites)).containsExactly(
-								false, false, false);
+		assertThat(extractProperty("isHost").from(invites)).containsExactly(
+				false, false, false);
 	}
-	
+
 	@Test
 	public void getInvitesAsParticipantSurveyNameTest() {
 		final List<LgInvite> invites = getInvitesAsParticipantFor("Bob");
-		assertThat(
-				extractProperty("survey.name").from(
-						invites)).containsOnly(
-								"Sporting Event", "Swim Course", "Denim Jeans Reunion");
+		assertThat(extractProperty("survey.name").from(invites)).containsOnly(
+				"Sporting Event", "Swim Course", "Denim Jeans Reunion");
 	}
 
 	private List<LgInvite> getInvitesAsParticipantFor(String username) {
-		final List<LgInvite> invites = new TestTransaction<List<LgInvite>>(username) {
+		final List<LgInvite> invites = new TestTransaction<List<LgInvite>>(
+				username) {
 			@Override
 			public List<LgInvite> execute() {
 				return startSession().getInvitesAsParticipant();
@@ -139,61 +161,60 @@ public class LgInviteTest {
 		}.getResult();
 		return invites;
 	}
-	
+
 	@Test
 	public void updateInviteSetConcreteAvailabilityTest() {
-		final LgInvite inviteForEvaluation = updatePossibleTimePeriodsWith(invites.get(0), 20, 40, 60);
+		updatePossibleTimeperiodsSportingSurvey(20, 40, 60);
 		assertThat(
-				extractProperty("durationMins").from(inviteForEvaluation.getPossibleTimePeriods()))
-					.containsOnly(
-							20, 40, 60
-							);
+				extractProperty("durationMins").from(
+						sportingSurveyInvite.getPossibleTimePeriods()))
+				.containsOnly(20, 40, 60);
 	}
-	
+
 	/**
 	 * Note: The call to updateInvite is internally forwarded to saveInvite.
 	 */
 	@Test
 	public void updateInviteChangeConcreteAvailabilityTest() {
-		final LgInvite inviteWithTimeperiods = updatePossibleTimePeriodsWith(bobsFirstInvite, 20, 40, 60);
-		final LgInvite inviteForEvaluation = updatePossibleTimePeriodsWith(inviteWithTimeperiods, 20, 80, 60);
+		updatePossibleTimeperiodsSportingSurvey(20, 40, 60);
+		updatePossibleTimeperiodsSportingSurvey(20, 80, 60);
 		assertThat(
-				extractProperty("durationMins").from(inviteForEvaluation.getPossibleTimePeriods()))
-					.containsOnly(
-							20, 80, 60
-							);
+				extractProperty("durationMins").from(
+						sportingSurveyInvite.getPossibleTimePeriods()))
+				.containsOnly(20, 80, 60);
 	}
-	
+
 	@Test
 	public void updateInviteDeleteOneConcreteAvailabilityTest() {
-		final LgInvite inviteWithTimeperiods = updatePossibleTimePeriodsWith(bobsFirstInvite, 20, 40, 60);
-		final LgInvite inviteForEvaluation = updatePossibleTimePeriodsWith(inviteWithTimeperiods, 20, 60);
+		updatePossibleTimeperiodsSportingSurvey(20, 40, 60);
+		updatePossibleTimeperiodsSportingSurvey(20, 60);
 		assertThat(
-				extractProperty("durationMins").from(inviteForEvaluation.getPossibleTimePeriods()))
-					.containsOnly(
-							20, 60
-							);
+				extractProperty("durationMins").from(
+						sportingSurveyInvite.getPossibleTimePeriods()))
+				.containsOnly(20, 60);
 	}
-	
+
 	@Test
 	public void updateInviteDeleteAllConcreteAvailabilitiesTest() {
-		final LgInvite inviteWithTimeperiods = updatePossibleTimePeriodsWith(bobsFirstInvite, 20, 40, 60);
-		final LgInvite inviteForEvaluation = updatePossibleTimePeriodsWith(inviteWithTimeperiods);
-		assertThat(
-				inviteForEvaluation.getPossibleTimePeriods())
-					.isEmpty();
+		updatePossibleTimeperiodsSportingSurvey(20, 40, 60);
+		updatePossibleTimeperiodsSportingSurvey();
+		assertThat(sportingSurveyInvite.getPossibleTimePeriods()).isEmpty();
 	}
-	
-	private LgInvite updatePossibleTimePeriodsWith(final LgInvite inviteWithOid, int ... durations) {
-		inviteWithOid.setPossibleTimePeriods(testUtils.buildTimePeriods(durations));
-		return new TestTransaction<LgInvite>("Bob") {
+
+	/**
+	 * Updates Bob's sporting survey invites with given durations.
+	 */
+	private void updatePossibleTimeperiodsSportingSurvey(int... durations) {
+		sportingSurveyInvite.setPossibleTimePeriods(testUtils
+				.buildTimePeriods(durations));
+		sportingSurveyInvite = new TestTransaction<LgInvite>("Bob") {
 			@Override
 			public LgInvite execute() {
-				return startSession().updateInvite(inviteWithOid);
+				return startSession().updateInvite(sportingSurveyInvite);
 			}
 		}.getResult();
 	}
-	
+
 	@After
 	public void tearDown() {
 		testUtils.deleteAccountsFor("Alice", "Bob", "Carol", "Denise", "Edgar");
