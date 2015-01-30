2631f28c61905ce61b08f139f10692af44e079f1
Sebastian Dassé <sebdas@web.de>
2015-01-30 11:20:41 +0100

    A slightly better build script. Starting cleanup for myApp.

--- prototype/de.bht.comanche/build.sh
+++ prototype/de.bht.comanche/build.sh
@@ -1,17 +1,33 @@
+#!/bin/sh
+
+if [ "$1" = "-h" -o "$1" = "--help" ]
+then
+    echo
+    echo "Usage: build.sh [OPTION]"
+    echo
+    echo "Options:"
+    echo "  -h, --help     display this help and exit"
+    echo "  -n, --no-docs  build without documentation generation for the webapp"
+    exit 0
+fi
+
+
 echo building plan the jam ...
 
-##-- server build --##
+###-- server build --###
 
 mvn clean install
 
-##-- webapp build --##
+###-- webapp build --###
 cd ./src/main/webapp/
 
 npm install
 
-## generate javascript docs if build script was called with option -d ##
-if [ "$1" != "-n" -o "$1" != "--no-docs" ]
+## generate webapp docs ##
+if [ "$1" = "-n" -o "$1" = "--no-docs" ]
 then
+    echo skipped documentation generation for the webapp ...
+else
     yuidoc .
 fi
 
